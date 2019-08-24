b97b3cb2436bab25945769a43c21f4462dc3ac7a
simohamed2018 <simoadam1408@gmail.com>
2019-08-24 17:22:16 -0400

    add tata file

---
+++ testfolder/tata.rb
@@ -0,0 +1,6 @@
+bonjour test sur branche testbranching
+
+dev branch modififcation pour merge first commit allo testing
+grader les deux lignes ici aussi rebase 
+add rebase line 
+modification dans testing branch pour conflit first commit allo testing

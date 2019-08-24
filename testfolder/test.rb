2eddfe697b5445555b83ca6a9bc4f16f2c0f485f
simohamed2018 <simoadam1408@gmail.com>
2019-08-24 17:31:51 -0400

    add fix rebase

--- testfolder/test.rb
+++ testfolder/test.rb
@@ -1,5 +1,10 @@
 bonjour test sur branche testbranching
 
 dev branch modififcation pour merge first commit allo testing
+
 grader les pour supprimer mergetool deux lignes
+
+grader les deux lignes ici aussi rebase 
+add rebase line 
+ test rebase
 modification dans testing branch pour conflit first commit allo testing

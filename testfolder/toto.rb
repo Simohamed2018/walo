af617288f953ca2defebf4258d88f868cdccad40
simohamed2018 <simoadam1408@gmail.com>
2019-08-24 17:21:03 -0400

    add toto

---
+++ testfolder/toto.rb
@@ -0,0 +1,5 @@
+bonjour test sur branche testbranching
+
+dev branch modififcation pour merge first commit allo testing
+grader les deux lignes
+modification dans testing branch pour conflit first commit allo testing

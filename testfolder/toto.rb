94651447066a6d1cf0698ed71dc1760e8f85cd27
simoadam <simoadam1408@gmail.com>
2019-11-17 09:17:55 -0500

    Revert "add toto"
    This reverts commit af617288f953ca2defebf4258d88f868cdccad40.

--- testfolder/toto.rb
+++
@@ -1,5 +0,0 @@
-bonjour test sur branche testbranching
-
-dev branch modififcation pour merge first commit allo testing
-grader les deux lignes
-modification dans testing branch pour conflit first commit allo testing
